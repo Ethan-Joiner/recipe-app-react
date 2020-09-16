326c8e38a9932f6830bc3e1434800537fb0d9d4b
Ethan <ethan.joiner@gmail.com>
2020-09-16 10:59:38 -0500

    testing push

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import './App.css';
 
 function App() {
   // Props can only be passed DOWN
-  const name = 'developedbyed'
+
+  const APP_ID = 'f6a9cd4a';
+  const APP_KEY = '84ae16841851398d497569fba7ca46dc';
+
+  const exampleReq = 
+  `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
   return (
     <div className="App">
       <h1>Howdy</h1>
-      <div className="home">
-    
-      </div>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
