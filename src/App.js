04fd016265463101a4ed29904cc3f69972b498c0
Ethan <ethan.joiner@gmail.com>
2020-09-16 09:56:25 -0500

    restarting states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+// Import Components
+
 
 function App() {
+  // Props can only be passed DOWN
+  const name = 'developedbyed'
   return (
     <div className="App">
       <h1>Howdy</h1>
+      <div className="home">
+    
+      </div>
     </div>
   );
 }
